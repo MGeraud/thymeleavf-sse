81ef46520b8f666a3078ebd33009d5b637af9307
MGeraud <geraud.mon@gmail.com>
2021-04-28 12:35:46 +0200

    change hard coded url for heroku test

--- src/main/resources/static/fizzbuzz.js
+++ src/main/resources/static/fizzbuzz.js
@@ -1,6 +1,6 @@
 window.addEventListener('load' , function (e){
     var fizzbuzzblock = document.getElementById('updateBlock'); //récup en variable de l'élément (fragment de thymeleaf)
-    var eventSource = new EventSource('http://localhost:8080/fizzbuzz-stream'); // recup de la source du flux
+    var eventSource = new EventSource('https://fuzzbizz.herokuapp.com/fizzbuzz-stream'); // recup de la source du flux
     eventSource.addEventListener('message' , function (update){
         fizzbuzzblock.innerHTML = update.data;
     });
